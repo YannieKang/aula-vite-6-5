c292b580633befe0279f9ad65c088338d70e2b9c
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-06 13:01:28 -0300

    aulaVite

---
+++ src/componentes/Header/Header.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar"
+
+function Header() {
+    return(
+      <header>
+        <span>Ola, Visitante!</span>
+        <h1>Bem-vindo ao MovieApp</h1>
+        <Navbar/>
+      </header>
+    )
+}
+export default Header
---
+++ src/componentes/Navbar/Navbar.jsx
@@ -0,0 +1,6 @@
+function Navbar() {
+    return(
+      <h1>Menu</h1>
+    )
+}
+export default Navbar
